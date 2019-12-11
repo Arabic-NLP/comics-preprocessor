16b93ef328b66fac494e0a26a0d54ae80b7fe33c
Noha Magdy <n.magdy@nu.edu.eg>
2019-12-12 00:17:08 +0200

    not working yet

--- src/commands/addDescription.py
+++ src/commands/addDescription.py
@@ -1,7 +1,19 @@
     
 class addDescription(commandBase):
     def prepare(self):
+        self.parameters=self.parameters.split(",")
+        UNI= unifyLanguage()
+        token= tokenizer()
+        detect=wordDetector()
+        x = self.parameters        
         return
     
+   
     def execut(self):
-        return
+        UNI.phrase=x
+        x= UNI.allinone()
+        x= token.splt(x)
+        x= stoppingWordsRemover(x)
+        x= token.formater(x)
+        x= detect.removeIfNotExist(x)
+        return x
